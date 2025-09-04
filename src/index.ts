1266b7e1b50ad53859314816149d7edb397f39e6
arifaeva <arifa.eva.ixa@gmail.com>
2025-09-04 23:15:12 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// type inferring
+const app = new Elysia()
+  // Hooks
+
+  // Routes
+  .get("/", () => "Hello Elysia")
+
+  // Port
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
